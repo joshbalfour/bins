027fc97c7520c42d377d2e14a73ee3864aa650e5
Josh Balfour <josh@joshbalfour.co.uk>
2022-06-17 19:30:16 +0100

    fix api url

--- packages/bins-app/src/graphql.ts
+++ packages/bins-app/src/graphql.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 // const uri = 'https://10.0.1.28:4010'
-const uri = 'https://bins.joshbalfour.co.uk'
+const uri = 'https://bins.joshbalfour.co.uk/api'
 
 export const client = new ApolloClient({
   uri,
